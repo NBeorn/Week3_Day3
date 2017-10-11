01d9713a97a932749a89709f2fb3db94524346d9
NBeorn <nmbeorn@gmail.com>
2017-10-11 14:11:23 -0400

    changed fields to protected, used them in auto class

--- Week3_Day3/Automobile.cs
+++ Week3_Day3/Automobile.cs
@@ -8,6 +8,21 @@ namespace Week3_Day3
 {
     class Automobile : Vehicle
     {
+        private int numberOfWheels;
+        private float wheelSize;
+        private int numberOfDoors;
 
+        public int NumberOfWheels
+        {
+            get { return this.numberOfWheels; }
+        }
+
+        public Automobile (int numberOfWheels, float wheelSize, int numberOfDoors, int seats, int carryingCapacity, string color, double movementSpeed)
+        {
+            this.numberOfWheels = numberOfWheels;
+            this.wheelSize = wheelSize;
+            this.numberOfDoors = numberOfDoors;
+
+        }
     }
 }
